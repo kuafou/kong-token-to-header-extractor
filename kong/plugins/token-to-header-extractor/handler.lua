532d617db581b03fa786dccd884ade72bfc3bf75
kuafu <kuafou@gmail.com>
2020-04-22 19:38:21 +0800

    Update handler.lua

--- kong/plugins/token-to-header-extractor/handler.lua
+++ kong/plugins/token-to-header-extractor/handler.lua
@@ -28,7 +28,6 @@ function TokenToHeaderExtractorHandler:access(conf)
             return nil
         end
         local header = ngx.req. get_headers()[entity.token_name]
-        kong.log.info("headeraaaa: " .. header)
         if header then
             local jwt, err = jwt_decoder:new(header)
             if err then
@@ -38,8 +37,6 @@ function TokenToHeaderExtractorHandler:access(conf)
             
             local claims = jwt.claims
             for claim_key, claim_value in pairs(claims) do
-                kong.log.info("token value: " .. entity.token_value_name)
-                kong.log.info("claim_key: " .. claim_key)
                 if entity.token_value_name == claim_key then
                     -- set header
                     req_set_header(entity.header_name, claim_value)
